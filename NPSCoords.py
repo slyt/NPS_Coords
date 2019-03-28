241b6b7af796b526c735302e39a228db13f74255
slyt <slyt@users.noreply.github.com>
2019-03-28 07:18:43 +0200

    Nearest Neighbor algorithm implemented by generating a linked list route.

--- NPSCoords.py
+++ NPSCoords.py
@@ -1,5 +1,6 @@
 import csv;
 import sys;
+import random;
 from geopy.distance import geodesic;
 
 class Place: # A type of Linked List Node that also has coordinate and distance information
@@ -11,6 +12,12 @@ class Place: # A type of Linked List Node that also has coordinate and distance 
         self.nextPlace = None
         self.nearestDistance = sys.maxint
 
+    def distance_to(self, destination):
+        # Find distance between Route Tail and currPark
+        # currPark[1] = Latitude
+        # currPark[2] = Longitude
+        return geodesic((self.lat, self.long), (destination.lat, destination.long))
+
 
 class Route: # A Linked List comprised of places
     def __init__(self):
@@ -21,36 +28,47 @@ class Route: # A Linked List comprised of places
     def listprint(self):
         printval = self.headval
         while printval is not None:
-            print (printval.name, printval.lat, printval.long)
+            print (printval.name, printval.lat, printval.long, printval.nearestDistance)
             printval = printval.nextPlace
+
+    def add_place(self, place):
+
+        if self.headval is None: # if empty route, add starting place
+            self.headval = place
+            self.tailval = place
+
+        else: # update tail value to point to the new place
+            self.tailval.nextPlace = place
+            self.tailval = place
+        self.length = self.length + 1
 
 if __name__ == '__main__':
 
-    parks = [] # List of Tuples(Park Name, Long, Lat, Nearest Park)
+    parks = [] # List of Places
 
     shortestRoute = Route()
     # Load CSV of National Park Coordinates
     with open('NationalParkGPSCoords.csv') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
+        next(readCSV) # skip CSV header: Place Name, Latitude, Longitude
         for row in readCSV:
-            parks.append((row[0],row[1], row[2]))
-            #print(row[0],row[1],row[2],)
-
-
-    print parks[1][0] # Place Name
-    print parks[1][1] # Latitude
-    print parks[1][2] # Longitude
+            currPlace = Place()
+            currPlace.name = row[0]
+            currPlace.lat = row[1]
+            currPlace.long = row[2]
+            parks.append(currPlace)
 
     # TODO: Add user input to select starting point
     # Add the first place as the head of the Linked List
-    shortestRoute.headval = Place(name = parks[1][0], lat=parks[1][1], long=parks[1][2])
-    shortestRoute.tailval = shortestRoute.headval
-    shortestRoute.length = 1
+    startingPlace = random.choice(parks) # Choose a random starting place
+    #parks[0] = None # Remove starting place from the list
+    shortestRoute.add_place(startingPlace)
+
     nearestParkIndex = 0
     parks[nearestParkIndex] = None
 
-
-    while shortestRoute.length <= parks.__len__():
+    # Compute until we have processed all of the parks.
+    while shortestRoute.length < parks.__len__():
         # Initialize loop
         nearestPark = None
         nearestParkDistance = sys.maxint
@@ -58,37 +76,32 @@ if __name__ == '__main__':
         # Iterate through all parks and find the nearest park to the current tail of the Route
         for currPark in parks:
 
-            if shortestRoute.length is parks.__len__(): # We are done if all of the parks have been used
+            if shortestRoute.length is parks.__len__():  # We are done if all of the parks have been used
                 break
 
-            if currPark is not None: # Ignore parks we've already added to the route
+            if currPark is None:  # This park has already been added to the Route and should be skipped
+                continue
 
-                # Find distance between Route Tail and currPark
-                # currPark[1] = Latitude
-                # currPark[2] = Longitude
-                currDist = geodesic( (shortestRoute.tailval.lat, shortestRoute.tailval.long), (currPark[1],currPark[2]) )
+            # Find distance between Route Tail and currPark
+            currDist = shortestRoute.tailval.distance_to(currPark)
 
-                #print currDist
+            if nearestPark is None:  # Initial Condition: The first park is always the nearest
+                nearestPark = currPark
+                nearestParkDistance = currDist
+                next
 
-                if nearestPark is None: # Initial Condition: The first park is always the nearest
-                    nearestPark = currPark
-                    nearestParkDistance = currDist
-                    next
+            if currDist < nearestParkDistance:  # We found a nearer park than previously discovered
+                nearestPark = currPark
+                nearestParkDistance = currDist
 
-                if currDist < nearestParkDistance: # We found a nearer park than previously discovered
-                    nearestPark = currPark
-                    nearestParkDistance = currDist
-                    nearestParkIndex = currPark.index()
+        # Update ShortestRoute with the nearest Park
+        parks.remove(nearestPark)  # Remove the nearest park from the possible parks
+        shortestRoute.tailval.nextPlace = nearestPark  # Point last place to the nearest place
+        shortestRoute.tailval.nearestDistance = currDist
+        shortestRoute.tailval = nearestPark  # Make the nearest place the last place
 
-        # TODO: Need outer loop here to iterate until we run out of new parks
-        # Each iteration, append nearestPark to shortestRoute.tailval
-        parks[nearestParkIndex] = None # Remove the nearest park from the possible parks
-        shortestRoute.headval.nextPlace = shortestRoute.tailval
-        shortestRoute.tailval = Place(nearestPark[0], nearestPark[1], nearestPark[2]) # Add reference to nearest place
 
-    # Traverse parks and find distance to all other parks, keep track of shortest park name until we get to the end.
-    # When we reach the end, mark the park as used so
-
+    # Print Result
     shortestRoute.listprint()
 
 
